7e9c27b1f82749c6bd9e20e379134563866086d5
FlameFractal <vishg8@gmail.com>
2017-09-19 11:45:30 +0530

    Event calendar started

---
+++ Event-Calendar/js/events.js
@@ -0,0 +1,80 @@
+var events = {
+	"user_1":[
+		{
+			"title": "Event 1",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 2",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 3",
+			"day": "Tuesday"
+		},
+		{
+			"title": "Event 4",
+			"day": "Thursday"
+		},
+		{
+			"title": "Event 5",
+			"day": "Friday"
+		},
+		{
+			"title": "Event 6",
+			"day": "Sunday"
+		},
+	],
+	"user_2":[
+		{
+			"title": "Event 1",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 2",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 3",
+			"day": "Tuesday"
+		},
+		{
+			"title": "Event 4",
+			"day": "Thursday"
+		},
+		{
+			"title": "Event 5",
+			"day": "Friday"
+		},
+		{
+			"title": "Event 6",
+			"day": "Sunday"
+		},
+	],
+	"user_3":[
+		{
+			"title": "Event 1",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 2",
+			"day": "Monday"
+		},
+		{
+			"title": "Event 3",
+			"day": "Tuesday"
+		},
+		{
+			"title": "Event 4",
+			"day": "Thursday"
+		},
+		{
+			"title": "Event 5",
+			"day": "Friday"
+		},
+		{
+			"title": "Event 6",
+			"day": "Sunday"
+		},
+	],
+}
